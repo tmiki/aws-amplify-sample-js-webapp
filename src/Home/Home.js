adfa2df459c2a78c88cbed125d762c6f85656419
datemaki <sidhemail@gmail.com>
2019-01-14 21:55:08 +0900

    added a new React Component called "Home".

---
+++ src/Home/Home.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './Home.css';
+
+class Home extends React.Component {
+    constructor() {
+        super();
+    }
+
+    render() {
+        return <div><h2>This is a "Home" component.</h2></div>;
+    }
+}
+
+export default Home;
